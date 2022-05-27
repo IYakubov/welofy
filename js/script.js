00ebfb7af5fb0d5646e72fc8ac825dd122d75461
IYakubov <45327728+IYakubov@users.noreply.github.com>
2022-05-27 15:30:09 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ var swiper = new Swiper(".home-slider", {
    },
 });
 
-var swiper = new Swiper(".food-slider", {
+var swiper = new Swiper(".models-slider", {
    grabCursor:true,
    loop:true,
    centeredSlides:true,
@@ -43,13 +43,13 @@ var swiper = new Swiper(".food-slider", {
    },
 });
 
-let previewContainer = document.querySelector('.food-preview-container');
-let previewBox = previewContainer.querySelectorAll('.food-preview');
+let previewContainer = document.querySelector('.models-preview-container');
+let previewBox = previewContainer.querySelectorAll('.models-preview');
 
-document.querySelectorAll('.food .slide').forEach(food =>{
-   food.onclick = () =>{
+document.querySelectorAll('.models .slide').forEach(models =>{
+   models.onclick = () =>{
       previewContainer.style.display = 'flex';
-      let name = food.getAttribute('data-name');
+      let name = models.getAttribute('data-name');
       previewBox.forEach(preveiw =>{
          let target = preveiw.getAttribute('data-target');
          if(name == target){
